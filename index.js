dbd08716e34656d49c647249ab7b2983ee288c57
ClesiriusGaming <40736194+ClesiriusGaming@users.noreply.github.com>
2018-12-21 21:33:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-bot.on("guildMemberAdd", =>
+bot.on("guildMemberAdd", => {
+       var nonverified = member.guild.roles.find(`NV`)
+member.addRole(nonverified)
+});
+
+bot.on(`message`, function(message) {
+  
+});
+
